02d2f821d45d602b27bf39bba72c648b65f1939e
puzzlewolf <nixos@nora.pink>
2026-05-27 11:31:25 +0200

    strictdoc: fix changelog location

--- pkgs/by-name/st/strictdoc/package.nix
+++ pkgs/by-name/st/strictdoc/package.nix
@@ -79,7 +79,7 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
   meta = {
     description = "Software for technical documentation and requirements management";
     homepage = "https://github.com/strictdoc-project/strictdoc";
-    changelog = "https://github.com/strictdoc-project/strictdoc/blob/${finalAttrs.src.tag}/CHANGELOG.md";
+    changelog = "https://strictdoc.readthedocs.io/en/stable/stable/docs/strictdoc_04_release_notes.html";
     license = lib.licenses.asl20;
     maintainers = [ lib.maintainers.puzzlewolf ];
     mainProgram = "strictdoc";
